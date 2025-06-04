ebe961ea38801b229f29b9823265e80121b28db0
robeeds <71895118+RobeeDS@users.noreply.github.com>
2025-06-04 11:58:19 -0700

    Added custom horizontal hero

--- src/heros/RenderHero.tsx
+++ src/heros/RenderHero.tsx
@@ -5,11 +5,13 @@ import type { Page } from '@/payload-types'
 import { HighImpactHero } from '@/heros/HighImpact'
 import { LowImpactHero } from '@/heros/LowImpact'
 import { MediumImpactHero } from '@/heros/MediumImpact'
+import { HorizontalHero } from '@/heros/Horizontal'
 
 const heroes = {
   highImpact: HighImpactHero,
   lowImpact: LowImpactHero,
   mediumImpact: MediumImpactHero,
+  horizontal: HorizontalHero,
 }
 
 export const RenderHero: React.FC<Page['hero']> = (props) => {
