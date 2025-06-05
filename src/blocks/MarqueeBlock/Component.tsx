781b34637604dc207d55d7c52242baf400399563
robeeds <71895118+robeeds@users.noreply.github.com>
2025-06-05 13:15:14 -0700

    Updated padding

--- src/blocks/MarqueeBlock/Component.tsx
+++ src/blocks/MarqueeBlock/Component.tsx
@@ -13,9 +13,9 @@ type Props = MarqueeBlockProps & {
 export const MarqueeBlock: React.FC<Props> = (props) => {
   const { richText, marqueeItems, staticImage } = props
   return (
-    <section className="container my-16">
+    <section className="grid container gap-y-8">
       <div>{richText && <RichText data={richText} enableGutter={false} />}</div>
-      <Marquee className="h-48 w-full overflow-hidden">
+      <Marquee className="w-full overflow-hidden">
         {marqueeItems?.map(({ media }, idx) => (
           <div key={idx} className="mx-4 shrink-0">
             <Media resource={media} src={staticImage} className="h-24 w-24 object-contain" />
