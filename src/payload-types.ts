57c11249a1f44087def2e3de5526a956e5da4581
robeeds <71895118+robeeds@users.noreply.github.com>
2025-06-08 20:50:16 -0700

    Rollback for storage plugin

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -287,6 +287,7 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -1573,6 +1574,7 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
