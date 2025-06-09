9027e6160d8f75e76df2cf0f176c48ed5d9498dc
robeeds <71895118+robeeds@users.noreply.github.com>
2025-06-08 18:41:51 -0700

    Update opengraph metadata

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,14 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: 'Management Information Systems student @ CSULB. Exploring how data reveals insight and how systems stay secure.',
   images: [
     {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
+      url: `${getServerSideURL()}/og.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'https://www.robeeds.dev',
+  title: 'RobeeDS | Data Analytics + Cybersecurity',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
