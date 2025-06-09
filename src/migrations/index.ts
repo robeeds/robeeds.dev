bc6d5e44947aed67945512a1d309952e319dfb2a
robeeds <71895118+robeeds@users.noreply.github.com>
2025-06-08 18:52:57 -0700

    Update migrations folder

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250609_015237 from './20250609_015237';
+
+export const migrations = [
+  {
+    up: migration_20250609_015237.up,
+    down: migration_20250609_015237.down,
+    name: '20250609_015237'
+  },
+];
